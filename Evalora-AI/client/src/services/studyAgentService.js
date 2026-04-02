ec76d51bf4bfbbd4ef585216cb25b336a5fb1d15
Anbuselvan-22 <anbuselvan22.info@gmail.com>
2026-04-02 09:14:32 +0530

    Done

--- Evalora-AI/client/src/services/studyAgentService.js
+++
@@ -1,6 +0,0 @@
-import apiClient from './apiClient';
-
-export const sendMessage = async (message, signal) => {
-  const response = await apiClient.post('/study-agent', { message }, { signal });
-  return response.data.data;
-};
